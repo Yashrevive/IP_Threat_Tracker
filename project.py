128440686dd45bede09cf90a8c0943138403fca7
Yashrevive <yashrevive@gmail.com>
2026-06-17 10:37:25 +0000

    Printing the location and other data of ip address

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ import ipaddress
 def main():
     argument = check_argument()
     ip_address = check_ipaddress(argument)
-
+    info_ipaddress(str(ip_address))
 
 
 def check_argument():
@@ -20,7 +20,28 @@ def check_ipaddress(argument):
     try:
         return(ipaddress.ip_address(argument))
     except ValueError:
-        return(f'Error: {argument} is not a valid IP address')
+        sys.exit(f'Error: {argument} is not a valid IP address')
+
+
+def info_ipaddress(ip_address):
+    try:
+        api = 'http://ip-api.com/json/'
+        response = requests.get(api+ip_address) 
+        response.raise_for_status()
+    except requests.RequestException:
+        sys.exit('Can\'t connect to the api')
+    else:
+        data = response.json()
+
+
+    if data['status'] == 'success':
+        print(f'''        IP: {ip_address}
+        Location: {data['city']},{data['regionName']},{data['country']},{data['zip']}
+        Internet service Provider: {data['isp']}
+        Autonomous System Number: {(data['as'].split())[0]} ''')
+    
+    else:
+        sys.exit(f'Error: {data['message']}')
 
 
 
